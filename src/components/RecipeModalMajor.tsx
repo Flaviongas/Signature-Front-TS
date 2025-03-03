2dd2edc1bdeb5f280401a6404174ba12ddacd1cc
ricardo <rjlopezdiaz@gmail.com>
2025-03-03 01:32:33 -0300

    creacion del formulario

--- src/components/RecipeModalMajor.tsx
+++ src/components/RecipeModalMajor.tsx
@@ -1,5 +1,9 @@
 import {
   Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -8,29 +12,47 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
-import React, { useContext } from "react";
+import React, { FormEvent, useContext, useRef } from "react";
 import MajorContext from "../contexts/MajorContext";
 
 type Props = { isOpen: boolean; onClose: () => void };
 
 function RecipeModalMajor({ isOpen, onClose }: Props) {
   const { selectedMajors } = useContext(MajorContext);
+  const nameRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(nameRef?.current?.value);
+    onClose();
+  };
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="6x1">
       <ModalOverlay />
-      <ModalContent maxW="90vw">
-        <ModalHeader>Agregar Asignatura</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody maxHeight="70vh" overflowY="auto">
-          {selectedMajors.name}
-        </ModalBody>
-        <ModalFooter>
-          <Button colorScheme="blue">Enviar Asistencia</Button>
-          <Button ml={3} onClick={onClose}>
-            Cerrar
-          </Button>
-        </ModalFooter>
-      </ModalContent>
+      <form onSubmit={handleSubmit}>
+        <ModalContent maxW="90vw">
+          <ModalHeader>Agregar Asignatura</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody maxHeight="70vh" overflowY="auto">
+            {/*selectedMajors.name*/}
+
+            <FormControl>
+              <FormLabel htmlFor="name">Ingresar Nombre</FormLabel>
+              <Input ref={nameRef} type="text" />
+              <FormHelperText>Ricardo estuvo aqui</FormHelperText>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button type="submit" colorScheme="blue">
+              Enviar Asistencia
+            </Button>
+
+            <Button ml={3} onClick={onClose}>
+              Cerrar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
     </Modal>
   );
 }
