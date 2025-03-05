f43627b420fbe01a58b39f7949c5487dd9db8632
flavio02 (NixOS) <flavyjala@hotmail.com>
2025-03-05 12:00:38 -0300

    style: fixed mainContent container

--- src/Routes/MainContent.tsx
+++ src/Routes/MainContent.tsx
@@ -30,12 +30,12 @@ function MainContent() {
           <div className="w-1/4 min-w-56 bg-blue-900 text-white max-w-72">
             <img src={logo} alt="logo" className="w-40 h-40 mx-auto my-2" />
             <SideNav />
-            <div className="felx text-center justify-center">
+            <div className="flex text-center justify-center">
               <Button className="flex my-3 align-middle justify-center text-center" onClick={logOut} >Cerrar sesión</Button>
             </div>
-            <div className="w-full bg-gray-100">
-              <Dashboard />
-            </div>
+          </div>
+          <div className="flex w-full bg-gray-100">
+            <Dashboard />
           </div>
         </div>
       </SubjectContext.Provider>
