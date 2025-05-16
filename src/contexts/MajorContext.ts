783d4467a5079e88001ebce5e08e5b7d92bc9a68
Manuel Ortega <dvuo@proton.me>
2025-05-16 14:21:55 -0400

    develop fix: remove commented-out function signature in MajorContextType

--- src/contexts/MajorContext.ts
+++ src/contexts/MajorContext.ts
@@ -4,6 +4,7 @@ import { MajorShort } from "../types";
 type MajorContextType = {
   selectedMajor: MajorShort;
   setSelectedMajor: (setSelectedMajor: MajorShort) => void;
+  // setSelectedMajor: (major: MajorShort) => void;
 };
 
 export default createContext<MajorContextType>({} as MajorContextType);
