db150079471b8173268d11145463a984e234fa46
Manuel Ortega <dvuo@proton.me>
2025-06-10 11:27:12 -0400

    develop chore: comment out localStorage mock setup in jest configuration

--- src/services/__tests__/jest.setup.ts
+++ src/services/__tests__/jest.setup.ts
@@ -1,12 +1,12 @@
-import jest
-const localStorageMock = {
-  getItem: jest.fn(() => "Token f31088828d62e0d39d98ae8dea2cfb2b3bf669b7"),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn(),
-};
+// import jest
+// const localStorageMock = {
+//   getItem: jest.fn(() => "Token f31088828d62e0d39d98ae8dea2cfb2b3bf669b7"),
+//   setItem: jest.fn(),
+//   removeItem: jest.fn(),
+//   clear: jest.fn(),
+// };
 
-Object.defineProperty(window, "localStorage", {
-  value: localStorageMock,
-  writable: true,
-});
+// Object.defineProperty(window, "localStorage", {
+//   value: localStorageMock,
+//   writable: true,
+// });
